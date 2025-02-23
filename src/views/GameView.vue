cc85c9cac1128d78dee157d7c2836ccf4e7e8dff
Toniogabe <antoniogabriel@outlook.com>
2025-02-23 16:37:44 -0500

    refact: change main name

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="bg-red-500 w-dvw h-dvh flex">
-    <span class="text-3xl m-auto">Game</span>
+  <main class="bg-blue-500 w-dvw h-dvh flex">
+    <span class="text-3xl m-auto">WiredGame</span>
   </main>
 </template>
